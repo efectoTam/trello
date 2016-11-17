1f66e1022e6b51d76997f3c33a3baa7e38d7a3de
Tamara Luque <tam.luque@gmail.com>
2016-11-16 23:25:17 -0300

    Vuelvo a hacer este ejercicio más ordenado y completo. Primer avance

--- js/main.js
+++ js/main.js
@@ -1,44 +1,60 @@
 function trello(){
-	var containerBotonesUno = document.getElementsByClassName("contenedorBotonUno")[0];
-  	var nuevaLista = document.createElement("div");
-  	var textoAgregarLista = document.createElement("textarea");
-  	var botonAgregarLista = document.createElement("button");
-  	var textoBotonUno= document.createTextNode("Guardar");
-  	botonAgregarLista.appendChild(textoBotonUno);
-  	textoAgregarLista.setAttribute("class", "textareaLista");
-  	textoAgregarLista.setAttribute("placeholder", "Ingresa el nombre de la lista")
-  	botonAgregarLista.setAttribute("class", "botonLista");
-  	nuevaLista.appendChild(textoAgregarLista);
-  	nuevaLista.appendChild(botonAgregarLista);
-  	containerBotonesUno.appendChild(nuevaLista);
-  
-  	var botondelaLista = document.getElementsByClassName("botonLista")[0];
+	var contenedorGrandeUno = document.getElementById('contenedorGrandeUno');
+	var ocultar = document.getElementById('añadirLista');
+	ocultar.classList.toggle('borrar');
+	var contenedorGuardar = document.createElement('div');
+	contenedorGuardar.setAttribute('id', 'guardarLista');
+	var textoAgregarLista = document.createElement("textarea");
+	textoAgregarLista.setAttribute("id", "textareaLista");
+	var botonAgregarLista = document.createElement("button");
+	botonAgregarLista.setAttribute("class", "botonLista");
+	var textoBotonUno= document.createTextNode("Guardar");
+	botonAgregarLista.appendChild(textoBotonUno);
+	contenedorGuardar.appendChild(textoAgregarLista);
+	contenedorGuardar.appendChild(botonAgregarLista);
+	contenedorGrandeUno.appendChild(contenedorGuardar);
 
-  	botondelaLista.addEventListener('click', function(){
-	    var nombreListaUsuario = document.getElementsByClassName("textareaLista")[0].value;
-	    var nodoLista = document.createTextNode(nombreListaUsuario);
-	    var containerTitulo = document.getElementsByClassName("contenedorTitulo")[0];
-	    var pDeTitulo = document.createElement("p");
-	    pDeTitulo.setAttribute("class", "estiloTitulo");
-	    pDeTitulo.appendChild(nodoLista);
-	    containerTitulo.appendChild(pDeTitulo);
-	    var contenedorParaBorrar= document.getElementsByClassName("estoSeBorra")[0];
-	    contenedorParaBorrar.style.display='none';
-	    var containerTarjetas = document.getElementsByClassName("contenedorTarjetas")[0];
-	    var botonParaLista = document.createElement("textarea");
-	    botonParaLista.setAttribute("class", "textareaAñadirTarjeta");
-	    containerTarjetas.appendChild(botonParaLista);
+	botonAgregarLista.addEventListener('click',function(){
+		var contenedorDeListas = document.getElementById('filaListas');
+		var nombreListaUsuario = document.getElementById('textareaLista').value;
 
-	    var botondelaTarjeta = document.getElementsByClassName("textareaAñadirTarjeta")[0];
-    
-    	botondelaTarjeta.addEventListener('click', function botonParaAñadir(){
-     		var elBotonAñadir = document.createElement("button");
-      		var textoBotonAñadir= document.createTextNode("Añadir tarjeta");
-      		elBotonAñadir.setAttribute("class", "botonTarjeta");
-      		elBotonAñadir.appendChild(textoBotonAñadir);
-      		containerTarjetas.appendChild(elBotonAñadir);
-      		botondelaTarjeta.removeEventListener('click', botonParaAñadir);
+		var todoListaIndividual = document.createElement('div');
+		todoListaIndividual.setAttribute('class', 'listaIndividual');
+		todoListaIndividual.setAttribute('class', 'col-xs-2 col-sm-2 col-md-2 col-lg-2');
 
+		var pDeTitulo = document.createElement("p");
+		pDeTitulo.setAttribute("class", "estiloTitulo");
+		var nodoLista = document.createTextNode(nombreListaUsuario);
+
+		var textLista = document.createElement("textarea");
+		textLista.setAttribute("class", "textareaAñadirTarjeta");
+
+		var botonAñadir = document.createElement("button");
+		botonAñadir.setAttribute("class", "botonTarjeta");
+		var textoBotonAñadir= document.createTextNode("Añadir tarjeta");
+
+		pDeTitulo.appendChild(nodoLista);
+		botonAñadir.appendChild(textoBotonAñadir);
+		todoListaIndividual.appendChild(pDeTitulo);
+		todoListaIndividual.appendChild(textLista);
+		todoListaIndividual.appendChild(botonAñadir);
+		contenedorDeListas.appendChild(todoListaIndividual);
+
+		botonAñadir.addEventListener('click', function(){
+
+		});
+
+
+
+
+
+
+	});
+
+
+}
+
+/*function trello(){
     		var botonParaTarjeta = document.getElementsByClassName("botonTarjeta")[0];
 
       		botonParaTarjeta.addEventListener('click', function (){
@@ -46,11 +62,11 @@ function trello(){
 	        	var nodoTarjeta = document.createTextNode(nombreTarjetaUsuario);
 	        	var containerTarjeta = document.getElementsByClassName("contenedorTarjetas")[0];
 	        	var pDeTarjeta = document.createElement("p");
-	        	pDeTarjeta.setAttribute("class", "estiloTarjeta");
+	        	pDeTarjeta.setAttribute("id", "estiloTarjeta");
 	        	pDeTarjeta.appendChild(nodoTarjeta);
 	        	var tareasDeTarjetas = document.getElementsByClassName("estiloTitulo")[0];
 	        	tareasDeTarjetas.appendChild(pDeTarjeta);
 			});
     	});
   	});
-}
+}*/
